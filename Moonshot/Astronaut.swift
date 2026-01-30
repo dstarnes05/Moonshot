a8eda86fd050d50c358f831d74320c1260a43566
Daniel Starnes <dstarnes261@gmail.com>
2026-01-30 16:25:33 -0600

    loaded json data into app; formatted main page

---
+++ Moonshot/Astronaut.swift
@@ -0,0 +1,14 @@
+//
+//  Astronaut.swift
+//  Moonshot
+//
+//  Created by Daniel Starnes on 1/30/26.
+//
+
+import Foundation
+
+struct Astronaut: Codable, Identifiable {
+    let id: String
+    let name: String
+    let description: String
+}
